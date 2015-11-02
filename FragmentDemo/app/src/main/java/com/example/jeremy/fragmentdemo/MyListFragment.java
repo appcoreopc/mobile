02820df8bcc4721592cfad8f0dcf06935eece858
jeremy <kepung@gmail.com>
2015-11-02 22:26:27 +0800

    fragment demo project check in

---
+++ FragmentDemo/app/src/main/java/com/example/jeremy/fragmentdemo/MyListFragment.java
@@ -0,0 +1,24 @@
+package com.example.jeremy.fragmentdemo;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Jeremy on 1/11/2015.
+ */
+public class MyListFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView.setText("My list ");
+        return rootView;
+    }
+}
+
